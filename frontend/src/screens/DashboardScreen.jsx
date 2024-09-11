64ca4b1b560d7882b7063f06bc559f65adb46a24
Ali Tariq <alibabapk@aol.com>
2024-09-11 11:50:41 +0500

    dashboard grid

--- frontend/src/screens/DashboardScreen.jsx
+++ frontend/src/screens/DashboardScreen.jsx
@@ -4,7 +4,18 @@ const DashboardScreen = () => {
   return (
     <div>
       <h1>Dashboard</h1>
-      
+      <div class="grid grid-cols-4 gap-4 border-4 rounded-lg ">
+      <div className='border-2 rounded-sm border-black' >01</div>
+      <div className='border-2 rounded-sm border-black' >02</div>
+      <div className='border-2 rounded-sm border-black' >03</div>
+      <div className='border-2 rounded-sm border-black' >04</div>
+      <div className='border-2 rounded-sm border-black' >05</div>
+      <div className='border-2 rounded-sm border-black' >06</div>
+      <div className='border-2 rounded-sm border-black' >07</div>
+      <div className='border-2 rounded-sm border-black' >08</div>
+      <div className='border-2 rounded-sm border-black' >09</div>
+      <div className='border-2 rounded-sm border-black' >10</div>
+    </div> 
     </div>
   )
 }
